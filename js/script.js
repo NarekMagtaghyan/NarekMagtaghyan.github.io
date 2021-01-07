7181e08ae484ec76e454a0f42ccdb45ecd6d278b
Narek Magtaghyan <narekmagtaghyan@gmail.com>
2021-01-07 23:32:54 +0400

    Adaptation of the first 2 blocks

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const menuWrapper = document.querySelector(".menu-icon-wrapper"),
+    menuIcon = document.querySelector(".menu-icon"),
+    navHolder = document.querySelector(".nav__list-holder");
+
+
+menuWrapper.addEventListener("click", () => {
+    menuIcon.classList.toggle("menu-icon-active");
+    navHolder.classList.toggle("nav__list-holder--visible");
+});
