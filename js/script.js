63617a935ed81de10a853c6f28ad5bace3bb35a0
Narek Magtaghyan <narekmagtaghyan@gmail.com>
2021-01-09 19:26:09 +0400

    The whole page is fully adapted

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
-const menuWrapper = document.querySelector(".menu-icon-wrapper"),
+window.addEventListener('DOMContentLoaded', () => {
+    const menuWrapper = document.querySelector(".menu-icon-wrapper"),
     menuIcon = document.querySelector(".menu-icon"),
     navHolder = document.querySelector(".nav__list-holder");
 
-
-menuWrapper.addEventListener("click", () => {
-    menuIcon.classList.toggle("menu-icon-active");
-    navHolder.classList.toggle("nav__list-holder--visible");
+    menuWrapper.addEventListener("click", () => {
+        menuIcon.classList.toggle("menu-icon-active");
+        navHolder.classList.toggle("nav__list-holder--visible");
+    });
 });
