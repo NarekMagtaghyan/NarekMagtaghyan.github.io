82cc2004132486584aa060111d4b99becf02feba
Narek Magtaghyan <narekmagtaghyan@gmail.com>
2021-01-10 19:41:28 +0400

    Fixed bug with navigation, added site icon

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 window.addEventListener('DOMContentLoaded', () => {
     const menuWrapper = document.querySelector(".menu-icon-wrapper"),
-    menuIcon = document.querySelector(".menu-icon"),
-    navHolder = document.querySelector(".nav__list-holder");
+        menuIcon = document.querySelector(".menu-icon"),
+        navHolder = document.querySelector(".nav__list-holder");
 
     menuWrapper.addEventListener("click", () => {
         menuIcon.classList.toggle("menu-icon-active");
         navHolder.classList.toggle("nav__list-holder--visible");
+
+        if (document.querySelector("body").style.overflow !== "hidden") {
+            document.querySelector("body").style.overflow = "hidden";
+        } else {
+            document.querySelector("body").style.overflow = "auto";
+        }
     });
 });
